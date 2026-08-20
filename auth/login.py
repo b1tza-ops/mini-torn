10f65b21240bf55e07029a894be42a24af5df85c
b1tza-ops <paul1mantech@gmail.com>
2026-08-20 22:40:27 +0100

    Added login and authenthicated game menu

---
+++ auth/login.py
@@ -0,0 +1,27 @@
+from database.users import get_user_by_username
+from utils.security import verify_password
+
+def login():
+    print("\n===== LOGIN =====")
+
+    username = input("Username: ").strip()
+    password = input("Password: ")
+
+    user = get_user_by_username(username)
+
+    if user is None:
+        print("User not found.")
+        return None
+
+    user_id = user[0]
+    stored_username = user[1]
+    password_hash = user[3]
+
+    if not verify_password(password, password_hash):
+        print("Incorrect password.")
+        return None
+
+    print("\nLogin successful!")
+    print("Welcome back,", stored_username)
+
+    return user_id
--- main.py
+++ main.py
@@ -1,20 +1,73 @@
 from database.setup import create_tables
 from auth.register import register
+from auth.login import login
 
 def main():
     create_tables()
+    while True: 
+        print("\n===== MINI TORN =====")
+        print("1. Login")
+        print("2. Register")
+        print("3. Forgot Password")
+        print("4. Exit")
 
-    print("\n===== MINI TORN =====")
-    print("1. Register")
-    print("2. Exit")
+        choice = input("Choose: ")
 
-    choice = input("Choose: ")
+        if choice == "1":
+            user_id = login()
 
-    if choice == "1":
-        register()
+            if user_id:
+                game_menu(user_id)
+                
+        elif choice == "2":
+            register()
 
-    elif choice == "2":
-        print("Goodbye!")
+        elif choice == "3":
+            print("Forgot password coming next")
+
+        elif choice == "4":
+            print("Goodbye!")
+            break
+
+        else:
+            print("Invalid option.")
+
+def game_menu(user_id):
+
+    while True:
+        print("\n===== GAME MENU =====")
+        print("1. Character")
+        print("2. Gym")
+        print("3. Crimes")
+        print("4. Jobs")
+        print("5. Inventory")
+        print("6. Logout")
+
+        choice = input("Choose: ")
+
+        if choice == "1":
+            print("Character coming soon.")
+
+        elif choice == "2":
+            print("Gym coming soon.")
+
+        elif choice == "3":
+            print("Crimes coming soon.")
+
+        elif choice == "4":
+            print("Jobs coming soon.")
+
+        elif choice == "5":
+            print("Inventory coming soon.")
+
+        elif choice == "6":
+            print("Logged out.")
+            break
+
+        else:
+            print("Invalid option.")
+
 
 if __name__ == "__main__":
     main()
+
